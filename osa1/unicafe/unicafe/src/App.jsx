7dab4fbadf2cc30744e8cca9cd5ce3b7820cf86d
Maria Idström <maria.idstrom@edu.turkuamk.fi>
2026-05-12 14:36:40 +0300

    t. 1.10 valmis

--- osa1/unicafe/unicafe/src/App.jsx
+++ osa1/unicafe/unicafe/src/App.jsx
@@ -1,6 +1,22 @@
 import { useState } from 'react'
 
-//uusi komponentti statistics, johon siirretään laskutoimitukset
+//button komponentti, joka saa propsina tekstin ja tapahtuman, joka tapahtuu klikkauksesta
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+//statisticsLine-komponentti, joka saa propsina tekstin ja arvon, ja näyttää ne muodossa "teksti arvo"
+const StatisticLine = (props) => {
+  return (
+    <p>
+      {props.text} {props.value}
+    </p>
+  )
+}
+//statistics-komponentti, joka saa propsina good, neutral ja bad, ja laskee niistä tilastot
 const Statistics = (props) => {
   const all = props.good + props.neutral + props.bad
 
@@ -20,35 +36,33 @@ const Statistics = (props) => {
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}%</p>
+      <StatisticLine text="good" value={props.good} />
+      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="bad" value={props.bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive + " %"} />
     </div>
   )
 }
 
+//App-komponentti, joka pitää tilan good, neutral ja bad, ja renderöi napit ja tilastot
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+
+//renderöidään napit button komponentilla
   return (
     <div>
       <h1>give feedback</h1>
 
       {/* Napit, jotka muuttavat tilaa klikkaamalla */}
-      <button onClick={() => setGood(good + 1)}>
-        good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        bad
-      </button>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+     
 
       {/*kutsutaan statistics-komponentti, jolle annetaan propsit*/}
       <Statistics good={good} neutral={neutral} bad={bad} />
