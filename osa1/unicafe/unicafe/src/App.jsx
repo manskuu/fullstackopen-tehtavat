71b32901558b8fece4dac6cc5796a23c17dd023f
Maria Idström <maria.idstrom@edu.turkuamk.fi>
2026-05-12 10:22:57 +0300

    t.1.7 valmis

--- osa1/unicafe/unicafe/src/App.jsx
+++ osa1/unicafe/unicafe/src/App.jsx
@@ -6,6 +6,11 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  //muuttujat laskukaavoille
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -27,6 +32,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
